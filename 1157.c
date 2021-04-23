d3c4c5c43782a2a4b711a92e18c3e0f8af9351b8
Newon <Newon_van@icloud.com>
2021-04-23 14:34:00 +0900

    문자열은 널 값도 생각하기

--- 1157.c
+++ 1157.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 
-int main()
+const char	*ft_alphabet()
 {
-	char s[1000000];
-	int i;
-	char str[26];
+	return "abcdefghijklmnopqrstuvwxyz";
+}
 
-	scnaf("%s", s);
+int		main()
+{
+	char str[1000001];
+	int i;
+	int j;
+	int h;
+	int count;
+	int flag;
+	const char *alphabet;
+
 	i = 0;
-	j = 1;
-	flag = 0;
-	tem = 0;
-	
-	while (s[i])
+	j = 0;
+	h = 0;
+	count = 0;
+	flag = -1;
+	scanf("%s", str);
+	alphabet = ft_alphabet();
+
+	while (alphabet[j])
 	{
-		
+		while (str[i])
+		{
+			if (alphabet[j] == str[i] || 
+				(alphabet[j] - ('a' - 'A')) == str[i])
+				count++;
+			i++;
+		}
+		if (flag == count)
+			h = -1;
+		if (flag < count)
+		{
+			flag = count;
+			h = j;
+		}
+		i = 0;
+		count = 0;
+		j++;
+	}
+	if (h == -1)
+	{
+		printf("?");
+		return (0);
+	}
+	printf("%c", alphabet[h] - ('a' - 'A'));
+	return (0);
+}
