cf1677edf10df08185660ad43ea9f5b7cb8ebf4e
Newon-universe <chokovon@gmail.com>
2021-12-06 23:54:24 +0900

    feat : 아기상어 풀었다

--- kotlin/풀었던 문제/16236_아기 상어.kt
+++ kotlin/풀었던 문제/16236_아기 상어.kt
@@ -1,64 +1,94 @@
 import java.util.*
 
-data class Coordinate(var row: Int, var cul: Int)
-data class Graph(var map: Int, var time: Int)
-lateinit var graph:Array<Array<Graph>>
-val direction = arrayOf(
-    Coordinate(-1, 0),
-    Coordinate(1, 0),
-    Coordinate(0, -1),
-    Coordinate(0, 1)
-    )
+data class Graph(var map: Int, var visit:Boolean, var time:Int)
+data class Coordinate(var row:Int, var cul: Int)
+lateinit var graph: Array<Array<Graph>>
+val direction = arrayOf(Coordinate(-1, 0), Coordinate(1, 0), Coordinate(0, -1), Coordinate(0, 1))
 
-fun clearTime(N: Int){
+fun clear(N: Int){
     for(i in 0 until N){
         for(j in 0 until N){
             graph[i][j].time = 0
+            graph[i][j].visit = false
         }
     }
 }
 
-fun bfs(start: Coordinate, size: Int, N: Int):Coordinate{
-    clearTime(N)
+fun bfs(N: Int, start: Coordinate, size: Int): Coordinate{
+    clear(N)
 
     var queue: Queue<Coordinate> = LinkedList()
-    var target = Coordinate(N, N)
-    var sharkSize = size
+    var now: Coordinate
+    var next: Coordinate = start
+    var targets: Queue<Coordinate> = LinkedList()
+    var result = start
 
     queue.add(start)
+    graph[start.row][start.cul].visit = true
 
     while(queue.isNotEmpty()){
-        var now = queue.poll()
+
+        now = queue.poll()
+
+        if(targets.isNotEmpty() && graph[targets.peek().row][targets.peek().cul].time <= graph[now.row][now.cul].time)
+            break
 
         for(i in 0..direction.lastIndex){
-            var next = Coordinate(now.row + direction[i].row, now.cul + direction[i].cul)
+            next = Coordinate(now.row + direction[i].row, now.cul + direction[i].cul)
 
             if(next.row < 0 || next.row >= N || next.cul < 0 || next.cul >= N)
                 continue
-            if(graph[next.row][next.cul].time > 0 || graph[next.row][next.cul].map > sharkSize) // sharkSize 에 의해 시작하는 지점을 밟지 않게된다. 참고로 N의 최대 크기는 20 -> 420개를 먹어야 사이즈 10 이 되는데, 최대 먹이수가 399개여서 넘지 못한다.
+            if(graph[next.row][next.cul].visit || graph[next.row][next.cul].map > size)
                 continue
-            if(graph[next.row][next.cul].map < sharkSize &&
-                graph[next.row][next.cul].map != 0 &&
-                graph[next.row][next.cul].time <= graph[now.row][now.cul].time &&
-                next.row < target.row && next.cul < target.cul ) {
-                target = next
+            if(graph[next.row][next.cul].map in 1 until size){
+                targets.add(next)
+                result = next
+                graph[next.row][next.cul].visit = true
+                graph[next.row][next.cul].time = graph[now.row][now.cul].time + 1
+                queue.add(next)
             }
-            graph[next.row][next.cul].time = graph[now.row][now.cul].time + 1
+
             queue.add(next)
+            graph[next.row][next.cul].time = graph[now.row][now.cul].time + 1
+            graph[next.row][next.cul].visit = true
         }
     }
 
-    for(i in 0 until N){
-        for(j in 0 until N){
-            if(target.row == i && target.cul == j)
-                print("${graph[i][j].time}* ")
-            else
-                print("${graph[i][j].time} ")
-        }
-        println()
+    for(i in targets){
+        if(i.row < result.row)
+            result = i
+        else if (i.row < result.row && i.cul < result.cul)
+            result = i
+        else if (i.row == result.row && i.cul < result.cul)
+            result = i
     }
 
-    return target
+    return result
+}
+
+fun solve(N: Int, Start: Coordinate):Int{
+    var size = 2
+    var eatenTimes = 0
+    var survivingTimes = 0
+    var target: Coordinate
+    var start = Start
+
+    while(true){
+        target = bfs(N, start, size)
+
+        if(target == start)
+            return survivingTimes
+
+        eatenTimes++
+        if(eatenTimes == size){
+            size++
+            eatenTimes = 0
+        }
+        graph[start.row][start.cul].map = 0
+        graph[target.row][target.cul].map = 9
+        start = target
+        survivingTimes += graph[target.row][target.cul].time
+    }
 }
 
 fun main(){
@@ -66,44 +96,23 @@ fun main(){
     val bw = System.`out`.bufferedWriter()
 
     val N = br.readLine().toInt()
-    graph = Array(N){ Array(N){ Graph(0, 0) } }
+    var start = Coordinate(N, N)
+    graph = Array(N){ Array(N){Graph(0, false, 0)} }
 
-    for(i in 0 until N){
-        graph[i] = br.readLine().split(" ").map { Graph(it.toInt(), 0) }.toTypedArray()
-    }
+    for(i in 0 until N)
+        graph[i] = br.readLine().split(" ").map{ Graph(it.toInt(), false, 0) }.toTypedArray()
 
 
-    var startPoint = Coordinate(0, 0)
-    loop@ for(i in 0 until N){
+    loop@for(i in 0 until N){
         for(j in 0 until N){
             if(graph[i][j].map == 9){
-                startPoint = Coordinate(i, j)
+                start = Coordinate(i, j)
                 break@loop
             }
         }
     }
-    var size = 2
-    var time = 0
-    var eatenNumber = 0
 
-    while(true){
-        var target = bfs(startPoint, size, N)
-
-        if(target.row == N && target.cul == N){
-            bw.append("$time")
-            bw.flush()
-            bw.close()
-            return
-        }
-
-        graph[target.row][target.cul].map = 0
-        time += graph[target.row][target.cul].time
-        startPoint = target
-        eatenNumber++
-        if(eatenNumber == size){
-            size++
-            eatenNumber = 0
-        }
-        println()
-    }
+    bw.write("${solve(N, start)}")
+    bw.flush()
+    bw.close()
 }
