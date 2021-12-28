0879bc416af0e9a25479a97cd141ec43a0f651f9
Newon-universe <chokovon@gmail.com>
2021-12-28 23:52:38 +0900

    feat : 데스나이트, DSLR

--- kotlin/풀었던 문제/10798_세로읽기.kt
+++ kotlin/풀었던 문제/10798_세로읽기.kt
@@ -1,23 +1,23 @@
-lateinit var input: Array<String>
-
-fun main(){
-    val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
-
-    input = Array(5){ "" }
-    for(i in 0..4){
-        input[i] = br.readLine()
-    }
-
-    var i = 0
-    for(cul in 0..14){
-        for(row in 0..4){
-            if(cul <= input[row].lastIndex  && input[row][cul].toString() != "")
-                bw.append("${input[row][cul]}")
-        }
-        i++
-    }
-
-    bw.flush()
-    bw.close()
-}
+//lateinit var input: Array<String>
+//
+//fun main(){
+//    val br = System.`in`.bufferedReader()
+//    val bw = System.`out`.bufferedWriter()
+//
+//    input = Array(5){ "" }
+//    for(i in 0..4){
+//        input[i] = br.readLine()
+//    }
+//
+//    var i = 0
+//    for(cul in 0..14){
+//        for(row in 0..4){
+//            if(cul <= input[row].lastIndex  && input[row][cul].toString() != "")
+//                bw.append("${input[row][cul]}")
+//        }
+//        i++
+//    }
+//
+//    bw.flush()
+//    bw.close()
+//}
--- kotlin/풀었던 문제/16948_데스 나이트.kt
+++ kotlin/풀었던 문제/16948_데스 나이트.kt
@@ -1,6 +1,55 @@
-fun main(){
-    val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
-
-
-}
+//import java.util.*
+//
+//data class Coordinate(var row: Int, var cul: Int)
+//lateinit var board:Array<Array<Int>>
+//
+//fun bfs(N: Int, r1: Int, c1: Int, r2: Int, c2: Int): Int{
+//    var queue: Queue<Coordinate> = LinkedList()
+//    var now: Coordinate
+//    var next: Coordinate
+//    var result = -1
+//    val direction = arrayOf(
+//        Coordinate(-2, -1),
+//        Coordinate(-2, 1),
+//        Coordinate(0, -2),
+//        Coordinate(0, 2),
+//        Coordinate(2, -1),
+//        Coordinate(2, 1))
+//    queue.add(Coordinate(r1,c1))
+//
+//    while(queue.isNotEmpty()){
+//        now = queue.poll()
+//
+//        for(i in direction){
+//            next = Coordinate(now.row + i.row, now.cul + i.cul)
+//
+//            if (next.row < 0 || next.row >= N || next.cul < 0 || next.cul >= N)
+//                continue
+//            if (board[next.row][next.cul] != 0)
+//                continue
+//            if (next.row == r2 && next.cul == c2){
+//                result = board[now.row][now.cul] + 1
+//                return result
+//            }
+//
+//            queue.add(Coordinate(next.row, next.cul))
+//            board[next.row][next.cul] = board[now.row][now.cul] + 1
+//        }
+//    }
+//
+//    return result
+//}
+//
+//
+//fun main(){
+//    val br = System.`in`.bufferedReader()
+//    val bw = System.`out`.bufferedWriter()
+//
+//    val N = br.readLine().toInt()
+//    board = Array(N){ Array(N){0} }
+//    val (r1,c1,r2,c2) = br.readLine().split(" ").map{it.toInt()}
+//
+//    bw.append("${bfs(N, r1, c1, r2, c2)}")
+//    bw.flush()
+//    bw.close()
+//}
