ad2d2704368acf380da018cc7979017ea082866f
Newon-universe <chokovon@gmail.com>
2022-01-11 23:26:02 +0900

    feat : 체스판 다시 칠하기 컷

--- kotlin/풀었던 문제/1018_체스판 다시 칠하기.kt
+++ kotlin/풀었던 문제/1018_체스판 다시 칠하기.kt
@@ -1,47 +1,50 @@
-//lateinit var graph: Array<Array<Char>>
-//
-//fun solve(startRow:Int, startCul: Int, N: Int, M: Int, standard: Int): Int {
-//    var countNow = 0
-//
-//    for(i in startRow..startRow + 7){
-//        for(j in startCul..startCul + 6){
-//            if (i >= N || j >= M)
-//                return -2
-//
-//            if ((j + 1 < M && graph[i][j] == graph[i][j + 1]) || (i + 1 < N && i != startRow + 7 && graph[i][j] == graph[i + 1][j])){
-//                countNow++
-//
-//            if (standard < countNow)
-//                return -1
-//        }
-//    }
-//
-//    return countNow
-//}
-//
-//fun main() {
-//    val br = System.`in`.bufferedReader()
-//    val bw = System.`out`.bufferedWriter()
-//
-//    val (N, M) = br.readLine().split(" ").map { it.toInt() }
-//    graph = Array(N) { Array(M) { '0' } }
-//    for (i in 0 until N)
-//        graph[i] = br.readLine().toCharArray().toTypedArray()
-//
-//    var standard = 0
-//    var temp: Int
-//    var result = 64
-//
-//    for (i in 0 until N){
-//        for (j in 0 until M){
-//            temp = solve(i, j, N, M, standard)
-//            if (temp == -1)
-//                standard++
-//            else if (temp < result && temp != -2)
-//                result = temp
-//        }
-//    }
-//    bw.append("$result")
-//    bw.flush()
-//    bw.close()
-//}
+import java.lang.Integer.min
+
+lateinit var graph: Array<Array<Char>>
+
+data class Coordinate(val row: Int, val cul: Int)
+
+fun solve(firstPosition: Coordinate, firstBoard: Char, N: Int, M: Int): Int {
+    var blackCount = 0
+    var whiteCount = 0
+
+    for (i in firstPosition.row..firstPosition.row + 7) {
+        for (j in firstPosition.cul..firstPosition.cul + 7) {
+            if (i >= N || j >= M)
+                return -1
+
+            if ((i + j) % 2 == 0 && graph[i][j] != firstBoard)
+                blackCount++
+            else if ((i + j) % 2 == 1 && graph[i][j] == firstBoard)
+                whiteCount++
+        }
+    }
+
+    return blackCount + whiteCount
+}
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    graph = Array(N) { Array(M) { '0' } }
+    for (i in 0 until N)
+        graph[i] = br.readLine().toCharArray().toTypedArray()
+
+    var standard = 64
+    var temp: Int
+    for (i in 0 until N) {
+        for (j in 0 until M) {
+            temp = solve(Coordinate(i, j), graph[i][j], N, M)
+            temp = min(temp, 64 - temp)
+
+            if (temp != -1 && temp < standard)
+                standard = temp
+        }
+    }
+
+    bw.append("$standard")
+    bw.flush()
+    bw.close()
+}
