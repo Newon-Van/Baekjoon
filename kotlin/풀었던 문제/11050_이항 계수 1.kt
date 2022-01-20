191692a30301016c5f2612f693bcd586ac548fd3
Newon-universe <chokovon@gmail.com>
2022-01-20 23:28:41 +0900

    feat : 분할정복

---
+++ kotlin/풀었던 문제/11050_이항 계수 1.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    val (N, K) = br.readLine().split(" ").map{it.toInt()}
+    
+
+
+}
--- kotlin/풀었던 문제/2798_블랙잭.kt
+++ kotlin/풀었던 문제/2798_블랙잭.kt
@@ -1,38 +1,38 @@
-var results = Array(3) { 0 }
-var result = 0
-var temp = 0
-lateinit var cards: Array<Int>
-lateinit var visit: BooleanArray
-
-fun backTracking(N: Int, M: Int, cardCount: Int) {
-    if (cardCount == 3) {
-        temp = results[0] + results[1] + results[2]
-        if (temp in (result + 1)..M)
-            result = temp
-        return
-    }
-
-    for (i in cards.indices) {
-        if(visit[i])
-            continue
-
-        results[cardCount] = cards[i]
-        visit[i] = true
-        backTracking(N, M, cardCount + 1)
-        results[cardCount] = 0
-        visit[i] = false
-    }
-}
-
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
-
-    val (N, M) = br.readLine().split(" ").map { it.toInt() }
-    cards = br.readLine().split(" ").map { it.toInt() }.toTypedArray()
-    visit = BooleanArray(N + 1) { false }
-    backTracking(N, M, 0)
-    bw.append("$result")
-    bw.flush()
-    bw.close()
-}
+//var results = Array(3) { 0 }
+//var result = 0
+//var temp = 0
+//lateinit var cards: Array<Int>
+//lateinit var visit: BooleanArray
+//
+//fun backTracking(N: Int, M: Int, cardCount: Int) {
+//    if (cardCount == 3) {
+//        temp = results[0] + results[1] + results[2]
+//        if (temp in (result + 1)..M)
+//            result = temp
+//        return
+//    }
+//
+//    for (i in cards.indices) {
+//        if(visit[i])
+//            continue
+//
+//        results[cardCount] = cards[i]
+//        visit[i] = true
+//        backTracking(N, M, cardCount + 1)
+//        results[cardCount] = 0
+//        visit[i] = false
+//    }
+//}
+//
+//fun main() {
+//    val br = System.`in`.bufferedReader()
+//    val bw = System.`out`.bufferedWriter()
+//
+//    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+//    cards = br.readLine().split(" ").map { it.toInt() }.toTypedArray()
+//    visit = BooleanArray(N + 1) { false }
+//    backTracking(N, M, 0)
+//    bw.append("$result")
+//    bw.flush()
+//    bw.close()
+//}
