ed06aef4215e2ab0ace4f21162d08fd1b6c833f7
Newon-universe <chokovon@gmail.com>
2022-01-24 21:57:42 +0900

    feat :

--- kotlin/풀었던 문제/1654_랜선 자르기.kt
+++ kotlin/풀었던 문제/1654_랜선 자르기.kt
@@ -1,55 +1,42 @@
-//import kotlin.time.ExperimentalTime
-//import kotlin.time.TimeSource
-//
-//lateinit var lanCable: Array<Int>
-//
-//fun slice(targetLanO: Int, minimumLanO: Int): Int {
-//    var targetLan = targetLanO
-//    var minimumLan = minimumLanO
-//
-//    var count = 0
-//
-//    while (targetLan > minimumLan) {
-//        targetLan -= minimumLan
-//        count++
-//    }
-//
-//    return count
-//}
-//
-//fun solve(K: Int, N: Int): Int {
-//    var minimumLan = lanCable[0]
-//    var count = 0
-//
-//    if (K == N)
-//        return minimumLan
-//
-//    while (minimumLan > 0) {
-//        for (i in lanCable.indices)
-//            count += slice(lanCable[i], minimumLan)
-//
-//        if (count == N)
-//            break
-//
-//        minimumLan--
-//        count = 0
-//    }
-//
-//    return minimumLan
-//}
-//
-//fun main() {
-//    val br = System.`in`.bufferedReader()
-//    val bw = System.`out`.bufferedWriter()
-//
-//    val (K, N) = br.readLine().split(" ").map { it.toInt() }
-//    lanCable = Array(K) { 0 }
-//    for (i in lanCable.indices)
-//        lanCable[i] = br.readLine().toInt()
-//
-//    lanCable.sort()
-//
-//    bw.append("${solve(K, N)}")
-//    bw.flush()
-//    bw.close()
-//}
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
+
+lateinit var lanCable: Array<Int>
+
+fun solve(K: Int, N: Int): Int {
+    var minimumLan = 0
+    var maximumLan = lanCable[lanCable.lastIndex] + 1
+    var mid: Int
+    var count:Long = 0
+
+    while (minimumLan < maximumLan){
+        mid = (maximumLan + minimumLan) / 2
+
+        for(i in lanCable.indices){
+            count += (lanCable[i] / mid)
+        }
+
+        if (count < N)
+            maximumLan = mid
+        else
+            minimumLan = mid + 1
+    }
+
+    return minimumLan
+}
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    val (K, N) = br.readLine().split(" ").map { it.toInt() }
+    lanCable = Array(K) { 0 }
+    for (i in lanCable.indices)
+        lanCable[i] = br.readLine().toInt()
+
+    lanCable.sort()
+
+    bw.append("${solve(K, N) - 1}")
+    bw.flush()
+    bw.close()
+}
