46d47d390f9e0ee988a66eb64c71189c6ac148fa
Newon-universe <chokovon@gmail.com>
2022-02-21 23:55:35 +0900

    feat : 돌그룹 컷

--- kotlin/풀었던 문제/12886_돌그룹_맞왜틀_이왜틀.kt
+++ kotlin/풀었던 문제/12886_돌그룹_맞왜틀_이왜틀.kt
@@ -5,59 +5,40 @@ data class Stones(var A: Int, var B: Int, var C: Int)
 fun bfs(A: Int, B: Int, C: Int): Int {
     if (A == B && B == C && C == A)
         return 1
+    else if ((A + B + C) % 3 != 0)
+        return 0
+
     var result = 0
 
     var queue: Queue<Stones> = LinkedList()
     var now: Stones
-    var next: Stones
-    var visit = Array(1000) { Array(1000) { Array(1000) { false } } }
+    val sum = A + B + C
+    var next: MutableList<Int>
+    var tempA: Int
+    var tempB: Int
+    var tempC: Int
+    var visit = Array(1501) { Array(1501) { false } }
     queue.add(Stones(A, B, C))
-    visit[A][B][C] = true
+    visit[A][B] = true
 
     while (queue.isNotEmpty()) {
         now = queue.poll()
-        next = now
+        next = mutableListOf(now.A, now.B, sum - now.A - now.B)
 
-        for (i in 1..3) {
-            when (i) {
-                1 -> {
-                    if (now.A == now.B)
-                        continue
-                    next = if (now.A > now.B)
-                        Stones(now.A - now.B, now.B + now.B, now.C)
-                    else
-                        Stones(now.A + now.A, now.B - now.A, now.C)
-                }
-                2 -> {
-                    if (now.B == now.C)
-                        continue
-                    next = if (now.B > now.C)
-                        Stones(now.A, now.B - now.C, now.C + now.C)
-                    else
-                        Stones(now.A, now.B + now.B, now.C - now.B)
-                }
-                3 -> {
-                    if (now.A == now.C)
-                        continue
-                    next = if (now.A > now.C)
-                        Stones(now.A - now.C, now.B, now.C + now.C)
-                    else
-                        Stones(now.A + now.A, now.B, now.C - now.A)
+        next.forEach { first ->
+            next.forEach { second ->
+                if (first < second && !visit[first * 2][second - first]) {
+                    tempA = first * 2
+                    tempB = second - first
+                    tempC = sum - tempA - tempB
+
+                    if (tempA == tempB && tempB == tempC && tempA == tempC)
+                        return 1
+
+                    visit[tempA][tempB] = true
+                    queue.add(Stones(tempA, tempB, tempC))
                 }
             }
-            if (next.A > 999 || next.B > 999 || next.C > 999 || next.A < 0 || next.B < 0 || next.C < 0)
-                continue
-
-            if (visit[next.A][next.B][next.C])
-                continue
-
-            if (next.A == next.B && next.B == next.C && next.A == next.C) {
-                result = 1
-                return result
-            }
-
-            queue.add(next)
-            visit[next.A][next.B][next.C] = true
         }
     }
 
