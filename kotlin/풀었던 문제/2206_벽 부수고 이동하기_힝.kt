4a4556ed3065991d220a34a1e35250fb87ad8e98
Newon-universe <chokovon@gmail.com>
2022-02-24 23:46:14 +0900

    feat : 벽 부수고 이동하기 컷

--- kotlin/풀었던 문제/2206_벽 부수고 이동하기_힝.kt
+++ kotlin/풀었던 문제/2206_벽 부수고 이동하기_힝.kt
@@ -1,60 +1,76 @@
 import java.util.*
 
-data class Graph(var map: Int, var position: Int)
-data class Coordinate(var row: Int, var cul: Int, var breakChance: Boolean)
-data class Visit(var normalVisit: Boolean, var chanceVisit: Boolean)
+data class Coordinate(var row: Int, var cul: Int, var canChance: Boolean, var time: Int)
+data class Visit(var normalVisit: Boolean, var chancedVisit: Boolean)
 
-lateinit var graph: Array<Array<Graph>>
+lateinit var graph: Array<Array<Int>>
 val directionArray = arrayOf(
-    Coordinate(-1, 0, true),
-    Coordinate(1, 0, true),
-    Coordinate(0, -1, true),
-    Coordinate(0, 1, true)
+    Coordinate(-1, 0, true, 1),
+    Coordinate(1, 0, true, 1),
+    Coordinate(0, -1, true, 1),
+    Coordinate(0, 1, true, 1)
 )
 
 fun bfs(N: Int, M: Int): Int {
     if (N == 1 && M == 1)
         return 1
-    var queue: Queue<Coordinate> = LinkedList()
+    val queue: Queue<Coordinate> = LinkedList()
+    var visit = Array(N) { Array(M) { Visit(normalVisit = false, chancedVisit = false) } }
     var now: Coordinate
     var next: Coordinate
-    var visit = Array(N) { Array(M) { Visit(false, false) } }
-    queue.add(Coordinate(0, 0, true))
-    graph[0][0].position = 1
-    visit[0][0].normalVisit = true
-    visit[0][0].chanceVisit = true
+    var nextGraph: Int
+    queue.add(Coordinate(0, 0, true, 1))
+    visit[0][0] = Visit(normalVisit = true, chancedVisit = true)
 
     while (queue.isNotEmpty()) {
         now = queue.poll()
 
         for (direction in directionArray) {
-            next = Coordinate(now.row + direction.row, now.cul + direction.cul, now.breakChance)
+            next = Coordinate(
+                now.row + direction.row, now.cul + direction.cul,
+                now.canChance, now.time + direction.time
+            )
 
-            if (next.row < 0 || next.row >= N || next.cul < 0 || next.cul >= M) // 벗어났어요??
+            // 범위 넘어가면 컷
+            if (next.row < 0 || next.row >= N || next.cul < 0 || next.cul >= M)
                 continue
 
-            if (graph[next.row][next.cul].map == 1 && (visit[next.row][next.cul].chanceVisit || !now.breakChance)) // 벽인데 부숴서 온 적 있거나 부술 기회 있나요??
+            // 도착하면 return
+            if (next.row == N - 1 && next.cul == M - 1)
+                return now.time + 1
+
+            nextGraph = graph[next.row][next.cul]
+
+            // 벽이 아닌데 능력써서도 방문해보고, 능력 안쓰고도 방문 해봤다면 컷
+            // 2번 오는거 어떻게 처리?
+            if (nextGraph == 0 && (visit[next.row][next.cul].normalVisit && visit[next.row][next.cul].chancedVisit))
                 continue
 
-            if (graph[next.row][next.cul].map == 0 && visit[next.row][next.cul].normalVisit && visit[next.row][next.cul].chanceVisit) // 벽 아닌데, 그냥으로도 부숴서도 온 적 있음?
+            // 벽인데 방문해봤거나, 벽을 뿌술 능력이 없다면 컷
+            if (nextGraph == 1 && (visit[next.row][next.cul].chancedVisit || !next.canChance))
                 continue
 
-            if (next.row == N - 1 && next.cul == M - 1)  // 정답인가요?
-                return graph[now.row][now.cul].position + 1
-
-
-            if (graph[next.row][next.cul].map == 1) {           // 벽이면, 벽부술 찬스 없애고 chancevisit 에 true 방문처리 핸들
-                next.breakChance = false
-                visit[next.row][next.cul].chanceVisit = true
-            } else if (graph[next.row][next.cul].map == 0 && now.breakChance)  // 벽이 아닌데, 찬스 아직 안 썻으면 normalVisit 로 방문철;
-                visit[next.row][next.cul].normalVisit = true
-            else if (graph[next.row][next.cul].map == 0 && !now.breakChance) // 벽이 아닌데, 찬스 썻으면 chanveVisit 로 방문처리
-                visit[next.row][next.cul].chanceVisit = true
-
-            queue.add(next)
-            graph[next.row][next.cul].position = graph[now.row][now.cul].position + 1
+            when {
+                nextGraph == 1 && next.canChance -> {
+                    next.canChance = false
+                    queue.add(next)
+                    visit[next.row][next.cul] =
+                        Visit(normalVisit = visit[next.row][next.cul].normalVisit, chancedVisit = true)
+                }
+                nextGraph == 0 && next.canChance && !visit[next.row][next.cul].normalVisit -> {
+                    queue.add(next)
+                    visit[next.row][next.cul] =
+                        Visit(normalVisit = true, chancedVisit = visit[next.row][next.cul].chancedVisit)
+                }
+                nextGraph == 0 && !next.canChance && !visit[next.row][next.cul].chancedVisit -> {
+                    queue.add(next)
+                    visit[next.row][next.cul] =
+                        Visit(normalVisit = visit[next.row][next.cul].normalVisit, chancedVisit = true)
+                }
+            }
         }
     }
+
     return -1
 }
 
@@ -63,12 +79,11 @@ fun main() {
     val bw = System.`out`.bufferedWriter()
 
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
-    graph = Array(N) { Array(M) { Graph(0, 0) } }
+    graph = Array(N) { Array(M) { 0 } }
     for (i in 0 until N)
-        graph[i] = br.readLine().map { Graph(it.code - 48, 0) }.toTypedArray()
+        graph[i] = br.readLine().map { it.code - 48 }.toTypedArray()
 
-    val result = bfs(N, M)
-    bw.append("$result")
+    bw.append("${bfs(N, M)}")
     bw.flush()
     bw.close()
 }
